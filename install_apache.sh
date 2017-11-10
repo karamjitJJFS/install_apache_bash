1a3930ac106157cbb506f796f0250f4ee3c26183
karamjitJJFS <karamjit.kang@jjfoodservice.com>
2017-11-10 17:04:04 +0000

    update test page
    html page is now index.html

--- install_apache.sh
+++ install_apache.sh
@@ -4,8 +4,8 @@ apt-get -y update
 # install Apache2
 apt-get -y install apache2 
 
-# write some HTML
-echo \<center\>\<h1\>Test Site\</h1\>\<br/\>\<h2\>Can you see me\</h2\>\</center\> > /var/www/html/demo.html
+# create test page and text
+echo \<center\>\<h1\>Test Site\</h1\>\<br/\>\<h2\>Can you see me\</h2\>\</center\> > /var/www/html/index.html
 
 # restart Apache
 apachectl restart
